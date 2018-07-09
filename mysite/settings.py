c2a7b109f85314f56b412e5f6efbe88d3718e6cf
Manabu Kikuchi <16217397+kerobot@users.noreply.github.com>
2018-07-09 22:27:53 +0900

    Merge settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 LOGIN_REDIRECT_URL = '/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
